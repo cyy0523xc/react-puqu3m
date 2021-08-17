89477d74023adbc6266a7377ac63ee7d4fb73c1a
cyy0523xc <cyy0523xc@qq.com>
2021-08-17 15:32:48 +0800

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
